5ed474e209eccffacca19104f35d51c2a26bbf96
Jotave <joaovictor0123mo@gmail.com>
2024-08-27 07:55:55 -0300

    primeraVersão

--- scenes/game_won.gd
+++ scenes/game_won.gd
@@ -3,5 +3,5 @@ extends CanvasLayer
 func _on_restart_pressed():
 	get_tree().change_scene_to_file("res://main.tscn") # emite o sinal de restart com destino no main
 	
-func _on_quit_pressed():
+func _on_quit_pressed(): # botão para sair 
 	get_tree().quit()
